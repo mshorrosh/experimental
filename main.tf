fa7824593e53e6f1bf3eb3ea607273c74e7100bd
Matt Shorrosh <matt.shorrosh@avanade.com>
2019-09-17 13:18:45 -0400

    t1

--- main.tf
+++ main.tf
@@ -1,47 +1,58 @@
+provider "azurerm" {
+  version         = "=1.32.1"
+  subscription_id = "{$var.subscription_id}"
+  tenant_id       = "{$var.tenant_id}"
+  client_id       = "{$var.client_id}"
+  client_secret   = "{$var.client_secret}"
 
-            
-provider "azurerm" 
-{
-    version = "=1.32.1"
 }
-            
-resource "azurerm_resource_group" "S" 
-{
-    name     = "S"
-    location = "East US"
-    
-    tags = {
-        
-        
+
+terraform {
+  backend "remote" {
+    organization = "CSS"
+
+    workspaces {
+      name = "CSS"
     }
-            
-}
-
-                
-resource "azurerm_virtual_network" "vnet01" 
-{
-    name                = "vnet01"
-    resource_group_name = "${azurerm_resource_group.S.name}"
-    address_space       =  ["10.0.0.0/16"]
-    location            = "East US"   
-    
-    
-    tags = {
-        
-        
-    }
-            
+  }
 }
 
 
-resource "azurerm_subnet" "vnet01-sub1" 
-{
-    name                 = "sub1"
-    resource_group_name  = "${azurerm_resource_group.S.name}"
-    virtual_network_name = "${azurerm_virtual_network.vnet01.name}"
-    address_prefix       = "10.0.1.0/24"
-   
-    
+
+resource "azurerm_resource_group" "S" {
+  name     = "S"
+  location = "East US"
+
+  tags = {
+
+
+  }
+
 }
 
- 
+
+resource "azurerm_virtual_network" "vnet01" {
+  name                = "vnet01"
+  resource_group_name = "${azurerm_resource_group.S.name}"
+  address_space       = ["10.0.0.0/16"]
+  location            = "East US"
+
+
+  tags = {
+
+
+  }
+
+}
+
+
+resource "azurerm_subnet" "vnet01-sub1" {
+  name                 = "sub1"
+  resource_group_name  = "${azurerm_resource_group.S.name}"
+  virtual_network_name = "${azurerm_virtual_network.vnet01.name}"
+  address_prefix       = "10.0.1.0/24"
+
+
+}
+
+
