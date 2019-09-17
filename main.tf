3aa07b0a0452c84ffa735f16f9757ab2d1e4c4e9
Matt Shorrosh <matt.shorrosh@avanade.com>
2019-09-17 13:36:56 -0400

    t

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 provider "azurerm" {
   version         = "=1.32.1"
-  subscription_id = "{$var.subscription_id}"
-  tenant_id       = "{$var.tenant_id}"
-  client_id       = "{$var.client_id}"
-  client_secret   = "{$var.client_secret}"
+  subscription_id = "${var.subscription_id}"
+  tenant_id       = "${var.tenant_id}"
+  client_id       = "${var.client_id}"
+  client_secret   = "${var.client_secret}"
 
 }
 
