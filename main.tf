fa8bce9b87b31caf2146910ce1b510152d2ded48
Matt Shorrosh <matt.shorrosh@avanade.com>
2019-09-30 12:26:13 -0400

    adding sub4

--- main.tf
+++ main.tf
@@ -57,5 +57,21 @@ resource "azurerm_subnet" "vnet01-sub2" {
 
 }
 
+resource "azurerm_subnet" "vnet01-sub3" {
+  name                 = "sub1"
+  resource_group_name  = "${azurerm_resource_group.S.name}"
+  virtual_network_name = "${azurerm_virtual_network.vnet01.name}"
+  address_prefix       = "10.0.2.0/24"
+
+}
+
+
+resource "azurerm_subnet" "vnet01-sub4" {
+  name                 = "sub1"
+  resource_group_name  = "${azurerm_resource_group.S.name}"
+  virtual_network_name = "${azurerm_virtual_network.vnet01.name}"
+  address_prefix       = "10.0.3.0/24"
+
+}
 
 
